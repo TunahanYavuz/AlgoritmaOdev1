68cdb1c30001fe61ab044e938f90576711029c64
Tunahan Yavuz <rey_tuna@hotmail.com>
2023-12-07 02:10:26 +0300

    Latest Version

--- main.c
+++ main.c
@@ -1,39 +1,45 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 
 int main() {
-    int number1,number2,temporaryNumber,flag;
+    int number1,number2,temporaryNumber,flag;  //Değişkenleri atıyoruz.
     float sum,bolumSayisi;
-    printf("Aralarindaki Asal Sayilari Bulacagimiz 2 Tane Dogal Sayi Giriniz\n");
-    scanf("%d%d",&number1,&number2);
+    printf("Aralarindaki Asal Sayilari Bulacagimiz 2 Tane Sayi Giriniz\n");
+    first:
+    if(scanf("%d",&number1)!=1){        //Kullanıcıdan 1. değeri alıyoruz.
+        printf("Lutfen Sadece Sayi Giriniz\n1. Sayiyi Tekrar Giriniz\n");
+        fflush(stdin);
+        goto first;
+    }
+    second:
+    if(scanf("%d",&number2)!=1){        //Kullanıcıdan 2. değeri alıyoruz.
+        printf("Lutfen Sadece Sayi Giriniz\n2. Sayiyi Tekrar Giriniz\n");
+        fflush(stdin);
+        goto second;
+    }
 
-    if(number1>number2){
+    if(number1>number2){               //Girilen ilk sayı daha küçük değilse 2. sayıyla yerini değiştiriyoruz.
         number1=temporaryNumber;
         number1=number2;
         number2=temporaryNumber;
     }
-    if(number1<0){
-        printf("Yanlis Tuslama 0 dan Kucuk Sayi %d Giremezsiniz",number1);
-        return 1;}
-    for (int i = number1+1; i < number2; ++i) {
-        flag=0;
-        for (int j = 2; j <i ; ++j)
+    printf("Asal Sayilariniz\n-------------------------\n");
+    for (int i = number1; i <= number2; ++i) {  //for döngüsüyle (sayi1,sayi2) aralıklarını i değişkeniyle teker teker dönüyoruz.
+        flag=0;                                  //Her başa döndüğümüzde flag değerini sıfırlayıp diğer sayı için hazır hale getiriyoruz.
+        for (int j = 2; j <i ; ++j)             //for döngüsüyle j değişkenini 2 den başlatıp i' ye kadar artırıyoruz.
         {
-            if(i%j == 0)
+            if(i%j == 0)                        // Eğer i%j 0' a eşitse flag değişkenini 1 artırıyoruz.
             {
                 flag++;
             }
         }
-        if(flag==0&&i!=1)
+        if(flag==0&&i!=1&&i>0)                      //Eğer flag değişkeni 2. for döngüsünde artmamışsa
         {
-            bolumSayisi++;
-            printf("%d\n",i);
-            sum=sum+i;
+            bolumSayisi++;                     //bolumSayisi' ni 1 artırıyoruz,
+            printf("\t%d\n",i);
+            sum=sum+i;                         //Toplamı i kadar artırıyoruz.
         }
 
-    }
+    }                                          //Son olarak da mesajı yazıp istenilen değerleri giriyoruz.
     printf("\tAsal Sayilarin Toplami : %0.1f\n\tToplam Asal Sayi : %0.0f\n\tAsal Sayilarin Ortalamasi : %f",sum,bolumSayisi,sum/bolumSayisi);
-
-
-
-    return 0;
+   return 0;
 }
